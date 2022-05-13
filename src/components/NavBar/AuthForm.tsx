28b5222dae1a0f94f00358fa884ffb80ee1472d0
Jordan <spamadress94@hotmail.fr>
2022-05-13 21:38:44 +0800

    Finish auth form design

--- src/components/NavBar/AuthForm.tsx
+++ src/components/NavBar/AuthForm.tsx
@@ -3,92 +3,118 @@ import { signup, signin } from '../../actions/auth'
 
 export default function AuthForm({ p_isSignup, exitModal }) {
 	const [isSignup, setIsSignup] = useState(p_isSignup)
-    const [formData, setFormdata] = useState({username: '', password: '', repeatPassword: ''})
-
+	const [formData, setFormdata] = useState({
+		username: '',
+		password: '',
+		repeatPassword: '',
+	})
 
 	function onChangeUsername(event) {
 		const inputValue: string = event.target.value
-        setFormdata(prevData => ({...prevData, username: inputValue}))
+		setFormdata((prevData) => ({ ...prevData, username: inputValue }))
 	}
 
 	function onChangePassword(event) {
 		const inputValue: string = event.target.value
-        setFormdata(prevData => ({...prevData, password: inputValue}))
+		setFormdata((prevData) => ({ ...prevData, password: inputValue }))
 	}
 
 	function onChangeRepeatPassword(event) {
 		const inputValue: string = event.target.value
-        setFormdata(prevData => ({...prevData, repeatPassword: inputValue}))
+		setFormdata((prevData) => ({ ...prevData, repeatPassword: inputValue }))
 	}
 
-	async function handleSignup(formInfos){
+	async function handleSignup(formInfos) {
 		const data = await signup(formInfos)
-		if(data){
+		if (data) {
 			localStorage.setItem('User', JSON.stringify(data))
 			window.location.reload()
 		}
 	}
 
-	async function handleSignin(formInfos){
+	async function handleSignin(formInfos) {
 		const data = await signin(formInfos)
-		if(data){
+		if (data) {
 			localStorage.setItem('User', JSON.stringify(data))
 			window.location.reload()
 		}
 	}
 
 	function onSubmit(event) {
-        event.preventDefault()
+		event.preventDefault()
 		console.log('formData from form ; ', formData)
-        if(isSignup){
-            handleSignup(formData)
-        } else {
-            handleSignin(formData)
-        }
+		if (isSignup) {
+			handleSignup(formData)
+		} else {
+			handleSignin(formData)
+		}
 		resetForm()
-        exitModal()
+		exitModal()
 	}
 
 	function resetForm() {
-		setFormdata({username: '', password: '', repeatPassword: ''})
+		setFormdata({ username: '', password: '', repeatPassword: '' })
 	}
 
 	return (
-		<form>
-			<h3>{isSignup ? 'Signup' : 'Login'} form</h3>
-			<label htmlFor="username">Username </label>
-			<input id="username" onChange={onChangeUsername} value={formData.username} />
-			<label htmlFor="password">Password </label>
-			<input
-				type="password"
-				id="password"
-				onChange={onChangePassword}
-				value={formData.password}
-			/>
-			{isSignup && (
-				<>
-					<label htmlFor="repeat-password">Repeat Password </label>
+		<>
+			<h3 className="auth-form-title">{isSignup ? 'Signup' : 'Login'}</h3>
+			<form className="auth-form">
+				<div className="auth-form-text-field">
+					<input
+						id="username"
+						onChange={onChangeUsername}
+						value={formData.username}
+						required
+					/>
+					<span></span>
+					<label htmlFor="username">Username </label>
+				</div>
+				<div className="auth-form-text-field">
 					<input
 						type="password"
-						id="repeat-password"
-						onChange={onChangeRepeatPassword}
-						value={formData.repeatPassword}
+						id="password"
+						onChange={onChangePassword}
+						value={formData.password}
+						required
 					/>
-				</>
-			)}
-			<button className="btn" onClick={onSubmit}>
-				{isSignup ? 'Signup' : 'Login'}
-			</button>
-			<button
-				type="button"
-				className="btn"
-				onClick={() => {
-					setIsSignup((prev) => !prev)
-					resetForm()
-				}}
-			>
-				Switch to {isSignup ? 'Login' : 'Signup'}
-			</button>
-		</form>
+					<span></span>
+					<label htmlFor="password">Password </label>
+				</div>
+				{isSignup && (
+					<div className="auth-form-text-field">
+						<input
+							type="password"
+							id="repeat-password"
+							onChange={onChangeRepeatPassword}
+							value={formData.repeatPassword}
+							required
+						/>
+						<span></span>
+						<label htmlFor="repeat-password">Repeat Password </label>
+					</div>
+				)}
+				<div>
+					<button className="btn auth-btn" onClick={onSubmit}>
+						{isSignup ? 'Signup' : 'Login'}
+					</button>
+					<div className="switch-login_signup">
+						{isSignup
+							? 'Already have an account ?'
+							: "Don't have an account yet ?"}
+						<button
+							type="button"
+							className="auth-link"
+							onClick={() => {
+								setIsSignup((prev) => !prev)
+								resetForm()
+							}}
+						>
+							{isSignup ? 'Login' : 'Signup'}
+						</button>
+					</div>
+				</div>
+			</form>
+		</>
 	)
 }
--- src/improvements.md
+++ src/improvements.md
@@ -1,20 +1,18 @@
-- Finish design et make footer ; undo et clear en plain text btn ?
-- add login and make database user based
-- counter de green days ecrit dans le nom de l'habbit
+- Finish design : add cross pour close auth form modal ; et make footer ; undo et clear en plain text btn ?
+- counter de green days ecrit dans le nom de l'habbit (+add dans la data obv)
 - move le form input en last row du table ?
 - make un explanation / title area
-- Keep or remove 'day' dans each cell ?
-- finish refacto
+- Gerer les user auth req erreurs et display dans le form
+- Auth form btn disabled quand champs pas bien remplis ? Actually easy to do
+- finish refacto (maybe can refacto effects in setHabits by having a logic function returning the updated array)
 - add interfaces and models for the rest (formData, User, context etc)
-- undo clear ? maybe not
 - add prevArray (for undo) dans la data ?
-- Template habit si 0 habits : fix que ca save dans la DB ? (will check quand je fais habits par user)
 
 Improvements :
 - Make a second part "finished" habbits bellow, et faire un array de finished habbits rendered en bas, quand l'habit passe finished elle est add au finishedHabbitArray et vice versa si elle refail
 - put first habit with few days already done if habbits array nul in begining
 - get une favicon
+- Make un truc pour move up/down habits in the list
 - make algorythm rules parametrables ?
 - Add un counter de added streak en darkred/finished
 - Make name editable (popup edit habit onclick sur le name ?) (ou alors contenteditable + manually onChange qui update in state & db ?)
-+ maybe add arrows to move up/down habits in the list
