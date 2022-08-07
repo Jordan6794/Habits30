352128c57b64f68d352f3806ddd6fc56fc22081b
Jordan <spamadress94@hotmail.fr>
2022-08-07 16:38:08 +0700

    Fix switch back API url to hosted backend

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { Habit } from '../components/Main/Table/habits.model'
 import { FormData, GoogleAuthData } from '../components/Auth/formData.model'
 
-const API = axios.create({baseURL: process.env.REACT_APP_LOCAL_BACKEND_BASE_URL})
+const API = axios.create({baseURL: process.env.REACT_APP_BACKEND_BASE_URL})
 
 API.interceptors.request.use((req) => {
     if(req.headers){
