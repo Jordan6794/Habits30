ba1b3e8671a74b2dbb1bd655449062d3f6ccbd5c
Jordan <spamadress94@hotmail.fr>
2022-08-09 14:05:32 +0700

    Update landing In Action preview screenshots

--- src/components/LandingPage/InActionArea.tsx
+++ src/components/LandingPage/InActionArea.tsx
@@ -10,15 +10,15 @@ const InActionArea: FunctionComponent = () => {
             <div className={styles.laptopDiv}>
                 <img className={`${styles.laptop}`}src='macbook.png' alt='habits in action' />
                 <div className={styles.laptopDivMargin}>
-                    <img className={`${styles.imgScreenshot} ${styles.img1}`}src='dashboard.png' alt='habits in action' />
-                    <img className={`${styles.imgScreenshot} ${styles.img2}`} src='localhost.png' alt='habits in action' />
-                    <img className={`${styles.imgScreenshot} ${styles.img3}`}src='habitsChrome.png' alt='habits in action' />
-                    {/* <img className={`${styles.imgScreenshot} ${styles.img4}`}src='localhost_3000_signup_phone.png' alt='habits in action' /> */}
+                    <img className={`${styles.imgScreenshot} ${styles.img1}`}src='scrnHabits.png' alt='habits in action' />
+                    <img className={`${styles.imgScreenshot} ${styles.img2}`} src='scrnDashboard.png' alt='habits in action' />
+                    <img className={`${styles.imgScreenshot} ${styles.img3}`}src='scrnHow.png' alt='habits in action' />
+
                     <img className={`${styles.phoneMockup}`} src='phone.png' alt='phone mockup' />
                     <img className={`${styles.phoneStatusBarMockup}`} src='phone-status-bar.jpg' alt='phone mockup' />
-                    <img className={`${styles.imgScreenshotPhone} ${styles.img1}`}src='dashboard.png' alt='habits in action' />
-                    <img className={`${styles.imgScreenshotPhone} ${styles.img2}`} src='localhost_3000_signup_phone.png' alt='habits in action' />
-                    <img className={`${styles.imgScreenshotPhone} ${styles.img3}`}src='habitsChrome.png' alt='habits in action' />
+                    <img className={`${styles.imgScreenshotPhone} ${styles.img1}`}src='scrnHabitsPhone.png' alt='habits in action' />
+                    <img className={`${styles.imgScreenshotPhone} ${styles.img2}`} src='scrnDashboardPhone.png' alt='habits in action' />
+                    <img className={`${styles.imgScreenshotPhone} ${styles.img3}`}src='scrnHowPhone.png' alt='habits in action' />
                 </div>
             </div>
         </div>
